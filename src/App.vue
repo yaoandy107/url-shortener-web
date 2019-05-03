c9a9e75cffc9683633a96f81a8b2f12bdad7a09c
yaoandy107 <yaoandy107@gmail.com>
2019-05-03 20:14:03 +0800

    Change font

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,8 @@ body {
   margin: 0;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir Next", Helvetica, Arial, "文泉驛正黑", "黑體-繁",
+    "微軟正黑體", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
